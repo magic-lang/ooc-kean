92c30cac95ed18afe67939226b5b388498ed82aa
Emil Westergren <emilwestergren@gmail.com>
2014-10-07 14:34:59 +0200

    Removed lib folder replaced with include folder

--- source/OpenGLES3/lib/EGLImage/EGLImageImport.h
+++
@@ -1,6 +0,0 @@
-#ifndef EGLIMAGEIMPORT_H_
-extern int createEGLImage(void* display);
-extern void destroyEGLImage(int index);
-extern void* lockPixels(int index);
-extern void unlockPixels(int index);
-#endif /* EGLIMAGEIMPORT_H_ */
