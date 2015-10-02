5b349ce66e224be07cd534c3af5daf96438fde39
Jonathan Udd <jonathan.udd@vidhance.com>
2015-10-02 08:13:42 +0200

    Added install step for rock in windows

--- install_rock.sh
+++ install_rock.sh
@@ -1,6 +1,14 @@
-tmp="$(wget -O - https://github.com/cogneco/rock/releases/latest | grep -o '\<download/rock_.*deb\>')"
-wget https://github.com/cogneco/rock/releases/$tmp
-echo ${tmp##*/}
-dpkg-deb -x ${tmp##*/} .
-mv usr/bin/rock .
-mv usr/lib/fancy_backtrace.so .
+#!/bin/bash
+if [ "$(uname -o)" == "Msys" ]; then
+	tmp="$(wget -O - https://github.com/cogneco/rock/releases/latest | grep -o '\<download/rock_.*zip\>')"
+	wget https://github.com/cogneco/rock/releases/$tmp
+	echo ${tmp##*/}
+	7z e ${tmp##*/}
+else
+	tmp="$(wget -O - https://github.com/cogneco/rock/releases/latest | grep -o '\<download/rock_.*deb\>')"
+	wget https://github.com/cogneco/rock/releases/$tmp
+	echo ${tmp##*/}
+	# dpkg-deb -x ${tmp##*/} .
+	# mv usr/bin/rock .
+	# mv usr/lib/fancy_backtrace.so .
+fi
