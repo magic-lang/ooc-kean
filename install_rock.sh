31078e2cb9f7917d0174ba20d0c64eaaea36a7b2
Jonathan Udd <jonathan.udd@vidhance.com>
2015-10-13 08:32:26 +0200

    Fixed rock_install script where comments where added by mistake

--- install_rock.sh
+++ install_rock.sh
@@ -8,7 +8,7 @@ else
 	tmp="$(wget -O - https://github.com/cogneco/rock/releases/latest | grep -o '\<download/rock_.*deb\>')"
 	wget https://github.com/cogneco/rock/releases/$tmp
 	echo ${tmp##*/}
-	# dpkg-deb -x ${tmp##*/} .
-	# mv usr/bin/rock .
-	# mv usr/lib/fancy_backtrace.so .
+	dpkg-deb -x ${tmp##*/} .
+	mv usr/bin/rock .
+	mv usr/lib/fancy_backtrace.so .
 fi
