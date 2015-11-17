364e62e7a87f4f126db9d7e9ea6993d8ebff6ba6
Sebastian Baginski <sebastian.baginski@vidhance.com>
2015-11-17 15:32:46 +0100

    removed CLIParser

--- test/base/CLIParser.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rock -v -r CLIParserTest.ooc 
-#./CLIParserTest -v -r --gc=off --lphtread -m message -z -n 23
-#./CLIParserTest -v -r --gc=off --lpthread -z
-./CLIParserTest --version --run --gc=off  --message message1 -z -n 23 -p /vidproc/imageFiles/ --path /vidproc/imageFiles/ -l 3 --level 4
