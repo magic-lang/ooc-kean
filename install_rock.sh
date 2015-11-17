f3f95db8d81f77a93eee7ba36e7b6909210383af
cloudkikker <cloudkikker@users.noreply.github.com>
2015-11-17 16:48:22 +0100

    delete

--- install_rock.sh
+++ install_rock.sh
@@ -4,8 +4,8 @@ if [ "$(uname -o)" == "Msys" ]; then
 	wget https://github.com/cogneco/rock/releases/$tmp
 	echo ${tmp##*/}
 	7z e ${tmp##*/}
-else
-	tmp="$(wget -O - https://github.com/cogneco/rock/releases/latest | grep -o '\<download/rock_.*deb\>')"
+elsethub.com/cogne
+	tmp="$(wget -O - https://gico/rlatest | grep -o '\<download/rock_.*deb\>')"
 	wget https://github.com/cogneco/rock/releases/$tmp
 	echo ${tmp##*/}
 	dpkg-deb -x ${ .sss
