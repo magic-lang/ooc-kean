0f91ae8c3dd24a07d992565e0750847fa9937668
David Hesselbom <david.hesselbom@vidhance.com>
2015-11-20 09:00:24 +0100

    Renamed a variable in install_rock.sh.

--- install_rock.sh
+++ install_rock.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 if [ "$(uname -o)" == "Msys" ]; then
-	tmp="$(wget -O - https://github.com/cogneco/rock/releases/latest | grep -o '\<download/rock_.*zip\>')"
-	wget https://github.com/cogneco/rock/releases/$tmp
-	echo ${tmp##*/}
-	7z e ${tmp##*/}
+	latest="$(wget -O - https://github.com/cogneco/rock/releases/latest | grep -o '\<download/rock_.*zip\>')"
+	wget https://github.com/cogneco/rock/releases/$latest
+	echo ${latest##*/}
+	7z e ${latest##*/}
 else
-	tmp="$(wget -O - https://github.com/cogneco/rock/releases/latest | grep -o '\<download/rock_.*deb\>')"
-	wget https://github.com/cogneco/rock/releases/$tmp
-	echo ${tmp##*/}
-	dpkg-deb -x ${tmp##*/} .
+	latest="$(wget -O - https://github.com/cogneco/rock/releases/latest | grep -o '\<download/rock_.*deb\>')"
+	wget https://github.com/cogneco/rock/releases/$latest
+	echo ${latest##*/}
+	dpkg-deb -x ${latest##*/} .
 	mv usr/bin/rock .
 	mv usr/lib/fancy_backtrace.so .
 fi
