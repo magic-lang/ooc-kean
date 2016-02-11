0081605eba948f173e3322a72e16ff6d59d5ed22
Marcus Näslund <marcus.naslund@vidhance.com>
2016-02-11 15:30:48 +0100

    Added flag for avoiding browser instance on code coverage test

--- tools/codecoverage.sh
+++ tools/codecoverage.sh
@@ -15,12 +15,15 @@ lcov --quiet --remove test_coverage.info '/test/*' -o test_coverage.info
 rm -r coverage/
 genhtml --quiet -o coverage $INFO_FILE --num-spaces 4
 
-if which xdg-open > /dev/null
+if [[ $* != *--nobrowser* ]]
 then
-	xdg-open 'coverage/index.html'
-elif which gnome-open > /dev/null
-then
-	gnome-open 'coverage/index.html'
-else
-	echo "Could not detect the web browser to use."
+	if which xdg-open > /dev/null
+	then
+		xdg-open 'coverage/index.html'
+	elif which gnome-open > /dev/null
+	then
+		gnome-open 'coverage/index.html'
+	else
+		echo "Could not detect the web browser to use."
+	fi
 fi
