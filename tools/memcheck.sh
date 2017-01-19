8bfd61e2b55d04bb74b8e54867ec44fdc2ec2f37
Marcus Näslund <marcus.naslund@vidhance.com>
2017-01-19 12:00:46 +0100

    Test license

--- tools/memcheck.sh
+++ tools/memcheck.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 logFile="memcheck.valgrindlog"
 valgrind --leak-check=full --log-file=$logFile --suppressions=./tools/kean.supp --num-callers=500 ./Tests -n $@
-
+#Hello
 grep -A5 LEAK $logFile | grep -v LEAK
 grep -A4 Invalid $logFile
 
